45e55295de4c6d41cd6a06936981a20a3caa5642
fabio.fernandez@ifb.edu.br <outraperspectiva>
2024-03-06 16:16:23 -0300

    first commit

---
+++ frases.py
@@ -0,0 +1,21 @@
+
+import random
+from tkinter import messagebox, Tk, Label, Button
+
+frases = ["sim", "não", "talvez"]
+
+def responder():
+    resposta_label = Label(text=frases[random.randrange(3)])
+    resposta_label.grid(row=2, column=1, columnspan=2)
+
+
+janela = Tk()
+janela.title("Gerador de Frases")
+janela.config(padx=100, pady=10)
+frase_label = Label(text="faça sua pergunta")
+frase_label.grid(row=1, column=0)
+
+botao_sorteio = Button(text="Responder pergunta", width=36, command=responder)
+botao_sorteio.grid(row=4, column=1, columnspan=2)
+
+janela.mainloop()
